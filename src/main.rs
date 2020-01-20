60eeba53c1293d4f4a4f576fbdd2b22f0f9f8bb1
Chris Lankford <christopherlankford@gmail.com>
2020-01-19 21:38:46 -0800

    Basic tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let a = Tuple {
+        x: 4.3,
+        y: -4.2,
+        z: 3.1,
+        w: 1
+    };
+    println!("{}, {}, {}, {}", a.x, a.y, a.z, a.w);
+}
+
+struct Tuple {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: u8
 }
