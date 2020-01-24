3bf75e1c95ce445530c5ef8c491ad756aebf0356
Chris Lankford <christopherlankford@gmail.com>
2020-01-23 19:26:31 -0800

    Added floating point equality function with error tolerance

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,16 @@ fn vector(x: f32, y: f32, z: f32) -> Tuple {
     }
 }
 
+fn equal(a: f32, b: f32) -> bool {
+    const EPSILON: f32 = 0.00001;
+    let diff: f32 = a - b;
+    if diff.abs() < EPSILON {
+        true
+    } else {
+        false
+    }
+}
+
 struct Tuple {
     x: f32,
     y: f32,
@@ -55,5 +65,25 @@ mod tests {
             1, p.w,
             "The 'w' field of a point should be 1, value was {}", p.w
         );
+    }
+
+    #[test]
+    fn values_are_equal() {
+        let a: f32 = 1.222225;
+        let b: f32 = 1.222226;
+        assert_eq!(
+            true, equal(a, b),
+            "The values {} and {} should be equal = true, value was {}", a, b, equal(a,b)
+        )
+    }
+
+    #[test]
+    fn values_are_not_equal() {
+        let a: f32 = 0.00001;
+        let b: f32 = 0.000021;
+        assert_eq!(
+            true, !equal(a, b),
+            "The values {} and {} should be equal = false, value was {}", a, b, equal(a,b)
+        )
     }
 }
