00eedd01ac77b9abd3176440f1e7b4341f8db6ad
Chris Lankford <christopherlankford@gmail.com>
2020-06-23 18:07:17 -0700

    Added a transform property to sphere

--- src/ray_tracer/sphere.rs
+++ src/ray_tracer/sphere.rs
@@ -1,15 +1,42 @@
 use crate::ray_tracer::tuple::Tuple;
 use crate::ray_tracer::traits::object::Object;
+use crate::ray_tracer::matrix::Matrix;
 
 #[derive(PartialEq)]
 pub struct Sphere {
-    pub origin: Tuple
+    pub origin: Tuple,
+    pub transform: Matrix
 }
 
 impl Object for Sphere {
     fn new() -> Self {
         Sphere {
-            origin: Tuple::point(0.0, 0.0, 0.0)
+            origin: Tuple::point(0.0, 0.0, 0.0),
+            transform: Matrix::identity()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_transformation() {
+        let s = Sphere::new();
+        assert!(
+            s.transform == Matrix::identity(),
+            "The default transform for the sphere was not set correctly on construction."
+        )
+    }
+
+    #[test]
+    fn change_transformation() {
+        let mut s = Sphere::new();
+        s.transform = Matrix::translation(2.0, 3.0, 4.0);
+        assert!(
+            s.transform == Matrix::translation(2.0, 3.0, 4.0),
+            "The default transform for the sphere was not set correctly on construction."
+        )
     }
 }
